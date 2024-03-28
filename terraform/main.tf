de1a28399ad33332e9e2f249365fd9c32857273b
oliver2401 <oliver.1012@hotmail.com>
2024-03-27 19:54:35 -0600

    added backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "oliver-myapp-tf-s3-bucket"
+    key = "myapp/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
